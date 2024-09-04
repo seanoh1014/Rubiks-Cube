fadc9830a81357cc55fd7cddd09a34365d656f90
seanoh1014 <103906443+seanoh1014@users.noreply.github.com>
2024-09-04 02:35:24 -0500

    Create Runner.java

---
+++ Runner.java
@@ -0,0 +1,13 @@
+public class Runner {
+
+    public static void main (String[] args) {
+
+        Cube cube = new Cube();
+        System.out.println(cube);
+
+        cube.rotateCube(4);
+        // cube.rotateCube(2);
+        System.out.println(cube);
+        
+    }
+}
